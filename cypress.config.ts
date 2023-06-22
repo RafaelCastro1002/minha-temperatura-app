771971a8dceedcf68ccd7024146b5be92cb70cd5
Rafael Castro <rafael.castro1002@gmail.com>
2023-06-21 20:12:27 -0400

    feat: change cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,14 +6,17 @@ export default defineConfig({
       framework: "create-react-app",
       bundler: "webpack",
     },
+
+    video: false
   },
 
   env: {
-    WEATHER_API_URL: "http://dataservice.accuweather.com",
+    WEATHER_API_URL: "https://express-api-proxy-example.vercel.app",
     WEATHER_API_KEY: "9v7n5SUsjmhs4ZpmHw9lveA0cElmIiN9",
   },
 
   e2e: {
     baseUrl: "http://localhost:3000",
+    video: false
   },
 });
